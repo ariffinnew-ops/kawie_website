eb4e5d064cf27b0468999ebc43e0d4cd9100e8b1
kawie <ariffin.new@gmail.com>
2026-07-25 16:54:59 +0800

    chore: work sync 25/07/2026 16:54

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,10 +26,12 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/favicon.ico' },
+      { url: '/favicon.svg', type: 'image/svg+xml' },
       { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
     ],
     apple: '/apple-touch-icon.png',
   },
+  manifest: '/site.webmanifest',
 }
 
 export default function RootLayout({
